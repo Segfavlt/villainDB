f8499eb9aafee409545c93e3990bb5706869e25b
Jeff Schwanebeck <jschwanebeck@tuta.io>
2019-02-17 19:35:02 -0800

    Add sql to populate advanced classes
    Add sql to populate spy/muscle/tech

--- sql/seed-j.sql
+++ sql/seed-j.sql
@@ -121,4 +121,106 @@ insert into minion(grade, base) values(
    9
 );
 
+#######
+# Spy #
+#######
 
+insert into spy values(
+  3,
+  5,
+  'Taser Pen',
+);
+
+insert into spy values(
+  5,
+  1,
+  'Polyurethane cloak'
+);
+
+insert into spy values(
+  4,
+  7,
+  'Sonic Blaster'
+)
+
+insert into spy values(
+  9,
+  2,
+  'Hologram Projector'
+);
+
+insert into spy values(
+  11,
+  8,
+  'Laser Cutter'
+);
+
+
+##########
+# Muscle #
+##########
+
+
+insert into muscle values(
+  2,
+  9,
+  'MP5'
+);
+
+insert into muscle values(
+  6,
+  3,
+  'Barrett M82'
+);
+
+insert into muscle values(
+  13,
+  5,
+  'De-atomizer'
+);
+
+insert into muscle values(
+  17,
+  7,
+  'Plasma Cannon'
+);
+
+insert into muscle values(
+  20,
+  2,
+  'AK-47'
+);
+
+########
+# Tech #
+########
+
+insert into tech values(
+  19,
+  4,
+  "Hydospanner"
+);
+
+insert into tech values(
+  18,
+  8,
+  'Holographic scanner'
+);
+
+insert into tech values(
+  1,
+  2,
+  '3D Printer'
+);
+
+insert into tech values(
+  8,
+  5,
+  'Foam Steel'
+);
+
+insert into tech values(
+  14,
+  4,
+  'Temporal Manipulator'
+);
