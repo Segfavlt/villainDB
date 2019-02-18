0a3bb689b2cd2982d58b07f9f6394cb3909fefe7
Jeff Schwanebeck <jschwanebeck@tuta.io>
2019-02-17 18:37:49 -0800

    Add sql to populate minion table
    Added sql to populate minion table

---
+++ sql/seed-j.sql
@@ -0,0 +1,124 @@
+use villainDB;
+
+
+###########
+# Minions #
+###########
+
+insert into minion(grade, base) values(
+  'B',
+   1
+);
+
+insert into minion(grade, base) values(
+  'D',
+   8
+);
+
+insert into minion(grade, base) values(
+  'S',
+   6
+);
+
+insert into minion(grade, base) values(
+  'S',
+   1
+);
+
+insert into minion(grade, base) values(
+  'A',
+   7
+);
+
+insert into minion(grade, base) values(
+  'C',
+   5
+);
+
+insert into minion(grade, base) values(
+  'D',
+   4
+);
+
+insert into minion(grade, base) values(
+  'B',
+   10
+);
+
+insert into minion(grade, base) values(
+  'A',
+   9
+);
+
+insert into minion(grade, base) values(
+  'D',
+   2
+);
+
+insert into minion(grade, base) values(
+  'A',
+   4
+);
+
+insert into minion(grade, base) values(
+  'B',
+   3
+);
+
+insert into minion(grade, base) values(
+  'S',
+   5
+);
+
+insert into minion(grade, base) values(
+  'D',
+   1
+);
+
+insert into minion(grade, base) values(
+  'A',
+   4
+);
+
+insert into minion(grade, base) values(
+  'S'
+   10
+);
+
+insert into minion(grade, base) values(
+  'A',
+   8
+);
+
+insert into minion(grade, base) values(
+  'S',
+   2
+);
+
+insert into minion(grade, base) values(
+  'A',
+   9
+);
+
+insert into minion(grade, base) values(
+  'S',
+   7
+);
+
+insert into minion(grade, base) values(
+  'A',
+   4
+);
+
+insert into minion(grade, base) values(
+  'A',
+   1
+);
+
+
+insert into minion(grade, base) values(
+  'S',
+   9
+);
+
+
