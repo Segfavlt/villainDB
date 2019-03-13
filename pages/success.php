44fa959684a29b36b47e84b0e67a2f71d63da3a8
Jeff Schwanebeck <jschwanebeck@tuta.io>
2019-03-13 12:20:54 -0700

    Add logic to redirect based on role

--- pages/success.php
+++ pages/success.php
@@ -1,5 +1,22 @@
 success
 <?php
+switch ($_SESSION['role']) {
+  case 'villain':
+    header('Location: villaindb.com/villain/index.php');
+    exit;
+    break;
+  case 'boss':
+    header('Location: bossdb.com/villain/index.php');
+    exit;
+    break;
+  case 'minion':
+    header('Location: miniondb.com/villain/index.php');
+    exit;
+    break;
+}
 
-echo "<a href='resources/php/logout.php'>Logout</a>";
+}
+
+
+
 ?>
