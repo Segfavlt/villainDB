862f69a1356346c4ca611c088772de5882c9720d
Jeff Schwanebeck <jschwanebeck@tuta.io>
2019-03-13 15:11:06 -0700

    Change theme, start navbar
    Changed the theme to the latest bootstrap cdn, added initial (broken)
    navbar

--- pages/login.php
+++ pages/login.php
@@ -7,26 +7,28 @@
 <html lang="en">
 <head>
 
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, \
-    shrink-to-fit=no">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap (check for updates) -->
-    <link href="resources/css/bootstrap.css" rel="stylesheet" id="bootstrap-css">
-    <title>villainDB</title>
+  <!-- Bootstrap (check for updates) -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <title>villainDB</title>
 
 </head>
 
-<body>
+<body class="bg-dark text-light">
 <div class="container">
     <div class="row m-4">
 
         <div class="col">
         </div>
 
-        <div class="col-4">
-            <div class=" p-3 shadow rounded">
+        <div class="col-4 bg-secondary">
+            <div class="p-3 shadow rounded">
                 <div class="pt-3">
                     <img
                         src="resources/svg/villainDB.svg" alt="villainDB" height="46px"
@@ -48,12 +50,6 @@
                             placeholder="Password"  name="pwd">
                     </div>
 
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input"
-                        id="rememberCheckBox">
-                        <label class="form-check-label text-dark"
-                          for="rememberCheckBox">Remember me?</label>
-                    </div>
 <?php
 
   if (isset($_SESSION['loginerror'])) {
--- resources/php/hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function hash_pass($pass) {
-  return password_hash($pass, PASSWORD_ARGON2I,
-    ['memory_cost' => 2048, 'time_cost' => 4, 'threads' => 3]);
-}
-?>
