4c5021533c88ad307ad539352d6ddadec191c0e8
Jeff Schwanebeck <jschwanebeck@tuta.io>
2019-03-13 13:41:04 -0700

    Fix session redirect.
    Fixes session redirect by including session_start() at the top of the
    page

--- pages/success.php
+++ pages/success.php
@@ -1,21 +1,18 @@
 <?php
+session_start();
 switch ($_SESSION['role']) {
   case 'villain':
-    header('Location: villaindb.com/villain/index.php');
+    header('Location: villain/index.php');
     exit;
     break;
   case 'boss':
-    header('Location: bossdb.com/villain/index.php');
+    header('Location: boss/index.php');
     exit;
     break;
   case 'minion':
-    header('Location: miniondb.com/villain/index.php');
+    header('Location: minion/index.php');
     exit;
     break;
 }
-
-}
-
-
 
 ?>
