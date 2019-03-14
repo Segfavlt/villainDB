65c10cf8ab51bfeae2a6185df9ceaf9f5bddc916
Jeff Schwanebeck <jschwanebeck@tuta.io>
2019-03-14 13:50:09 -0700

    Check validity of session vars, add redirect
    Check to see if session vars are set on index/success, add redirect
    logic if not

--- html/index.php
+++ html/index.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-if ($_SESSION['authenticated']) {
+if (isset($_SESSION['authenticated']) && $_SESSION['authenticated']) {
   header('Location: /success.php');
   exit;
 }
--- html/success.php
+++ html/success.php
@@ -1,18 +1,24 @@
 <?php
 session_start();
-switch ($_SESSION['role']) {
-  case 'villain':
-    header('Location: villain/index.php');
-    exit;
-    break;
-  case 'boss':
-    header('Location: boss/index.php');
-    exit;
-    break;
-  case 'minion':
-    header('Location: minion/index.php');
-    exit;
-    break;
+if (isset($_SESSION['authenticated'])) {
+  switch ($_SESSION['role']) {
+    case 'villain':
+      header('Location: villain/index.php');
+      exit;
+      break;
+    case 'boss':
+      header('Location: boss/index.php');
+      exit;
+      break;
+    case 'minion':
+      header('Location: minion/index.php');
+      exit;
+      break;
+    default:
+      header('Location: /');
+      break;
+  }
+} else {
+  header('Location: /');
 }
-
 ?>
