b8c5a0351d4d19457c8c9f208cc3ebcd95091084
Jeff Schwanebeck <jschwanebeck@tuta.io>
2019-03-27 16:19:05 -0700

    Fix class display
    Changed class display from number->job

--- html/resources/php/minion_display.php
+++ html/resources/php/minion_display.php
@@ -7,7 +7,7 @@ if($mysql ->connect_errno) {
     echo 'Could not connect: ' . $mysql->connect_error;
 }
 
-$query = "select * from minion order by id";
+$query = "select minion.id, minion.grade, minion.base, advanced.class from minion left join advanced on minion.class=advanced.rank order by id";
 $result = $mysql -> query($query);
 
 while ($row = $result->fetch_assoc()):
@@ -17,7 +17,7 @@ while ($row = $result->fetch_assoc()):
   <th scope="row"> <?php echo $row['id']; ?> </th>
   <td> <?php echo $row['grade']; ?> </td>
   <td> <?php echo $row['base']; ?> </td>
-  <td> <?php echo $row['class']; ?> </td>
+  <td> <?php echo ucfirst($row['class']); ?> </td>
   <td class="text-right">
     <a href="../resources/php/minion_edit.php?delete=<?php echo $row['id']; ?> "
     class="btn btn-danger"><i class="fas fa-trash"></i></a>
